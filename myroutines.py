4979a3ab807a855d806e6bf9b0c0bf107cd55d23
Tonalli Rodriguez <t3rodrig@users.noreply.github.com>
2020-09-11 20:33:46 -0500

    Update routines

--- myroutines.py
+++ myroutines.py
@@ -1,7 +1,10 @@
 import re
+import pandas as pd
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import SnowballStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
 
 def clean_up(s, stop_words=stopwords.words('spanish'), stemmer=SnowballStemmer("spanish", ignore_stopwords=True)):
@@ -15,3 +18,41 @@ def clean_up(s, stop_words=stopwords.words('spanish'), stemmer=SnowballStemmer("
     new_list = [token for token in stem_list if token not in stop_words]
     new_string = " ".join(new_list)
     return new_string
+
+# Function that takes a description as input and outputs most similar product
+def get_recommendation(description, text_series, df):
+    description = clean_up(description)
+    tmp_series = pd.concat([
+        text_series,
+        pd.Series([description])
+    ], ignore_index=True)
+
+    vectorizer = TfidfVectorizer(
+        analyzer='word',
+        ngram_range=(1, 2),
+        min_df=0.003
+    )
+    
+    #Construct the required TF-IDF matrix by fitting and transforming the data
+    tfidf_matrix = vectorizer.fit_transform(tmp_series)
+
+    # Compute the cosine similarity matrix
+    cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+    
+    # Set the index of product description
+    idx = -1
+    
+    # Get the pairwsie similarity scores of all products with that product
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    
+    # Sort the products based on the scores
+    sim_scores = sorted(sim_scores, key=lambda x:x[1], reverse=True)
+    
+    # Get the score of the 10 most similar products
+    sim_scores = sim_scores[1:11]
+    
+    # Get the product indices
+    product_indices = [i[0] for i in sim_scores]
+    
+    # Return  the top most similar products
+    return df.iloc[product_indices]
